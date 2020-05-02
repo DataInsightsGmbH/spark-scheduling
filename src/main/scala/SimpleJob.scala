860522deff55661acf2a87333e0aa5dccd479817
Marin Ferara <marin@Marins-MBP.fritz.box>
2020-05-02 16:16:55 +0200

    created simple spark application

--- src/main/scala/SimpleJob.scala
+++ src/main/scala/SimpleJob.scala
@@ -1,3 +1,15 @@
-object SimpleJob{
+import org.apache.spark.sql.SparkSession
 
+object SimpleJob extends SparkSessionWrapper {
+  def main(args: Array[String]): Unit = {
+    val df = spark
+      .read
+      .option("header", true)
+      .option("multiline", true)
+      .csv("/marin/train.csv")
+
+    df.write
+      .mode("overwrite")
+      .parquet("marin/train1")
+  }
 }

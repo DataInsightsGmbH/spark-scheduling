24b9039053393f96a807eab145f959daa61c3232
marin123 <Token: 5816f4c73c794739c34e3d7bd30e6998c32009b4>
2020-05-05 22:34:36 +0200

    made one rune in ParallelExample much smaller

--- src/main/scala/ParallelExample.scala
+++ src/main/scala/ParallelExample.scala
@@ -8,9 +8,6 @@ object ParallelExample extends SparkSessionWrapper {
         val input1 = spark.sparkContext.parallelize(1 to 1000000000, 8)
         val evenNumbers = input1.filter(nr => {
           val isEven = nr % 2 == 0
-          if (nr == 180000) {
-            Thread.sleep(3000L)
-          }
           isEven
         })
         evenNumbers.count()
@@ -19,12 +16,10 @@ object ParallelExample extends SparkSessionWrapper {
     new Thread(new Runnable() {
       override def run(): Unit = {
         spark.sparkContext.setLocalProperty("spark.scheduler.pool", "fair")
-        val input2 = spark.sparkContext.parallelize(1000000000 to 2000000000, 8)
+        Thread.sleep(500)
+        val input2 = spark.sparkContext.parallelize(1000000000 to 1000010000, 8)
         val oddNumbers = input2.filter(nr => {
-          val isOdd = nr % 1000000 != 0
-          if (nr == 1500000000) {
-            Thread.sleep(3000L)
-          }
+          val isOdd = nr % 2 != 0
           isOdd
         })
         oddNumbers.count()
